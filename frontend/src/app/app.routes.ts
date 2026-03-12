8a5461e03dd64df9afcc4e24392780f4564100f1
DashP06 <dashzoop@gmail.com>
2026-03-12 16:00:07 -0700

    Pages and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Editor } from './pages/editor/editor';
+import { Admin } from './pages/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'editor', component: Editor },
+  { path: 'admin', component: Admin },
+];
